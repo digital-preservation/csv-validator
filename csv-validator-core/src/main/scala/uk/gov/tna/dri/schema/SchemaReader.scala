00ef1c6f977b69d3b91baf2f611dac013faa8428
Benjamin Parker <ben@benparker.co.uk>
2013-01-17 12:04:12 +0000

    [BP] [feature] - Check if schema file is readable
    git-svn-id: https://wb-d-tfs2.web.local/svn/dri/trunk/csv-validator@4136 6bcde575-cb03-8b4d-b1e0-7a084bef6d9c

--- csv-validator-core/src/main/scala/uk/gov/tna/dri/schema/SchemaReader.scala
+++ csv-validator-core/src/main/scala/uk/gov/tna/dri/schema/SchemaReader.scala
@@ -1,5 +1,9 @@
-package uk.gov.tna
+package uk.gov.tna.dri.schema
+
+import java.io.File
 
 object SchemaReader {
+  def fileReadable(path: String) = new File(path).canRead
+
   def fileExists(path: String) = false
 }
--- csv-validator-core/src/test/scala/uk/gov/tna/dri/schema/SchemaReaderSpec.scala
+++ csv-validator-core/src/test/scala/uk/gov/tna/dri/schema/SchemaReaderSpec.scala
@@ -1,7 +1,6 @@
 package uk.gov.tna.dri.schema
 
 import org.specs2.mutable._
-import uk.gov.tna.SchemaReader
 
 class SchemaReaderSpec extends Specification {
 
@@ -9,6 +8,10 @@ class SchemaReaderSpec extends Specification {
 
     "check if the file exists" in {
       SchemaReader.fileExists("/there/is/no/file/here.txt") must_== false
+    }
+
+    "check the file is readable" in {
+      SchemaReader.fileReadable("src/test/resources/schema.readable.txt") must_== true
     }
 
   }
