a41b226c910a30d352595a82cee6e53571991160
David Ainslie <dainslie@gmail.com>
2013-01-24 11:31:08 +0000

    [BP DA] [Feature] Added implicit conversion of String to StringReader for improved test readability.
    git-svn-id: https://wb-d-tfs2.web.local/svn/dri/trunk/csv-validator@4216 6bcde575-cb03-8b4d-b1e0-7a084bef6d9c

--- csv-validator-core/src/test/scala/uk/gov/tna/dri/csv/CsvValidatorSpec.scala
+++ csv-validator-core/src/test/scala/uk/gov/tna/dri/csv/CsvValidatorSpec.scala
@@ -1,21 +1,22 @@
 package uk.gov.tna.dri.csv
 
 import org.specs2.mutable.Specification
-import uk.gov.tna.dri.schema.{SchemaParser, Schema}
+import uk.gov.tna.dri.schema.SchemaParser
 import java.io.StringReader
 
 class CsvValidatorSpec extends Specification {
+
+  implicit def stringToStringReader(text: String) = new StringReader(text)
 
   val validator = new CsvValidator with SchemaParser
 
   "Validation" should {
     "succeed for correct total columns" in {
-
-      validator.validate(new StringReader("col1"), new StringReader("{ @TotalColumns 1 }")) mustEqual true
+      validator.validate("col1", "{ @TotalColumns 1 }") mustEqual true
     }
 
     "fail for incorrect number of total columns" in {
-      validator.validate(new StringReader("col1, col2"), new StringReader("{ @TotalColumns 1 }")) mustEqual false
+      validator.validate("col1, col2", "{ @TotalColumns 1 }") mustEqual false
     }
 
     "succeed for correct total columns for multiple lines" in {
@@ -23,7 +24,7 @@ class CsvValidatorSpec extends Specification {
         """col1, col2
            col1, col2"""
 
-      validator.validate(new StringReader(csv), new StringReader("{ @TotalColumns 2 }")) mustEqual true
+      validator.validate(csv, "{ @TotalColumns 2 }") mustEqual true
     }
 
     "fail for incorrect number of total columns for multiple lines" in {
@@ -31,7 +32,8 @@ class CsvValidatorSpec extends Specification {
         """col1, col2, col3
            col1, col2
            col1, col2, col3"""
-      validator.validate(new StringReader(csv), new StringReader("{ @TotalColumns 3 }")) mustEqual false
+
+      validator.validate(csv, "{ @TotalColumns 3 }") mustEqual false
     }
   }
 }
