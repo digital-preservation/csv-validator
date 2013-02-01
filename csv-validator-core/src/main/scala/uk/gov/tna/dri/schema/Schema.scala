f1578ecaf1a5e2461af8e19f832d4b0a51a7b296
Jim Collins <jim.r.collins@gmail.com>
2013-02-01 12:51:57 +0000

    [JC] - I have put back the Schema invariant that the number of column definitions should equal the total column number. This is good programming practice and if there is a compelling reason why we should not be checking invariants I will be happy to remove it.
    git-svn-id: https://wb-d-tfs2.web.local/svn/dri/trunk/csv-validator@4360 6bcde575-cb03-8b4d-b1e0-7a084bef6d9c

--- csv-validator-core/src/main/scala/uk/gov/tna/dri/schema/Schema.scala
+++ csv-validator-core/src/main/scala/uk/gov/tna/dri/schema/Schema.scala
@@ -1,3 +1,5 @@
 package uk.gov.tna.dri.schema
 
-case class Schema(totalColumns: Int, columnDefinitions: List[ColumnDefinition])
+case class Schema(totalColumns: Int, columnDefinitions: List[ColumnDefinition]) {
+  require(totalColumns == columnDefinitions.length, s"totalColumns: ${totalColumns} must be the same as the number of column definitions: ${columnDefinitions.size}")
+}
