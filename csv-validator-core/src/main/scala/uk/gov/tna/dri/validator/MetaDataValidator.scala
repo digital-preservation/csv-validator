820d04c2a301290e75f3f02ec64e721f92dc6ec0
Benjamin Parker <ben@benparker.co.uk>
2013-02-07 11:53:52 +0000

    [BP] - Using Cell, Row and MetaData to simplify the validation
    git-svn-id: https://wb-d-tfs2.web.local/svn/dri/trunk/csv-validator@4458 6bcde575-cb03-8b4d-b1e0-7a084bef6d9c

--- csv-validator-core/src/main/scala/uk/gov/tna/dri/validator/MetaDataValidator.scala
+++ csv-validator-core/src/main/scala/uk/gov/tna/dri/validator/MetaDataValidator.scala
@@ -12,40 +12,57 @@ trait MetaDataValidator {
   type MetaDataValidation[A] = ValidationNEL[String, A]
 
   def validate(csv: Reader, schema: Schema) = {
-    val rows = new CSVReader(csv).readAll()
+    val rows = new CSVReader(csv).readAll().toList
+    validateMetaData(MetaData.fromStrings(rows.map(_.toList)), schema)
+  }
 
-    val v = for ((row, rowIndex) <- rows.zipWithIndex) yield validateRow(rowIndex + 1, row.toList, schema)
+  private def validateMetaData(metaData: MetaData, schema: Schema) = {
+    val v = for (row <- metaData.rows) yield validateRow(row, schema)
     v.sequence[MetaDataValidation, Any]
   }
 
-  private def validateRow(lineNumber: Int, row: List[String], schema: Schema) = {
-    val totalColumnsV = totalColumns(lineNumber, row, schema)
-    val rulesV = rules(lineNumber, row, schema)
+  private def validateRow(row: Row, schema: Schema) = {
+    val totalColumnsV = totalColumns(row, schema)
+    val rulesV = rules(row, schema)
     (totalColumnsV |@| rulesV) { _ :: _ }
   }
 
-  private def totalColumns(lineNumber: Int, row: List[String], schema: Schema) = {
-    if (row.length == schema.totalColumns) true.successNel[String] else s"Expected @TotalColumns of ${schema.totalColumns} and found ${row.length} on line ${lineNumber}".failNel[Any]
+  private def totalColumns(row: Row, schema: Schema) = {
+    if (row.cells.length == schema.totalColumns) true.successNel[String] else s"Expected @TotalColumns of ${schema.totalColumns} and found ${row.cells.length} on line ${row.lineNumber}".failNel[Any]
   }
 
-  private def rules(lineNumber: Int, row: List[String], schema: Schema) = {
-    val columnDefWithValue = schema.columnDefinitions.zip(row)
+  private def rules(row: Row, schema: Schema) = {
+    val columnDefWithValue = schema.columnDefinitions.zip(row.cells)
     val myMap = columnDefWithValue.collect{
-      case (x,y) => (x.id , y)
+      case (colDef, cell) => (colDef.id , cell.value)
     } toMap
 
-    val cells = row.lift
-    val v = for { (columnDefinition, columnIndex) <- schema.columnDefinitions.zipWithIndex } yield validateCell(myMap, lineNumber, cells(columnIndex), columnDefinition)
+    val cells = row.cells.lift
+    val v = for { (columnDefinition, columnIndex) <- schema.columnDefinitions.zipWithIndex } yield validateCell(myMap, row.lineNumber, cells(columnIndex), columnDefinition)
 
     v.sequence[MetaDataValidation, Any]
   }
 
-  def validateCell(colToMap: Map[String, String], lineNumber: Int, cell: Option[String], columnDefinition: ColumnDefinition) = cell match {
+  def validateCell(colToMap: Map[String, String], lineNumber: Int, cell: Option[Cell], columnDefinition: ColumnDefinition) = cell match {
     case Some(c) => rulesForCell(colToMap, lineNumber,c, columnDefinition)
     case _ => s"Missing value at line: ${lineNumber}, column: ${columnDefinition.id}".failNel[Any]
   }
 
-  private def rulesForCell(colToMap: Map[String, String], lineNumber: Int, cell: String, columnDefinition: ColumnDefinition) = {
-    columnDefinition.rules.map(_.execute(lineNumber, colToMap, columnDefinition, cell)).sequence[MetaDataValidation, Any]
+  private def rulesForCell(colToMap: Map[String, String], lineNumber: Int, cell: Cell, columnDefinition: ColumnDefinition) = {
+    columnDefinition.rules.map(_.execute(lineNumber, colToMap, columnDefinition, cell.value)).sequence[MetaDataValidation, Any]
   }
+
+  case class MetaData (rows: List[Row])
+
+  object MetaData {
+    def fromStrings(rows: List[List[String]]) = new MetaData(rows.zipWithIndex.map{ case (r,i) => Row.fromStrings(r, i + 1) })
+  }
+
+  case class Row (cells: List[Cell], lineNumber: Int)
+
+  object Row {
+    def fromStrings(row: List[String], lineNumber: Int) = new Row(row map (Cell(_)), lineNumber)
+  }
+
+  case class Cell(value: String)
 }
