4f93f9e6da788fa5621217c65b0c28055916f925
Jim Collins <jim.r.collins@gmail.com>
2013-02-13 11:00:51 +0000

    [JC] -Added additional rules
    git-svn-id: https://wb-d-tfs2.web.local/svn/dri/trunk/csv-validator@4609 6bcde575-cb03-8b4d-b1e0-7a084bef6d9c

--- csv-validator-core/src/main/scala/uk/gov/tna/dri/schema/Rule.scala
+++ csv-validator-core/src/main/scala/uk/gov/tna/dri/schema/Rule.scala
@@ -7,3 +7,59 @@ import uk.gov.tna.dri.metadata.Row
 trait Rule {
   def execute(columnIndex: Int, row: Row, schema: Schema): ValidationNEL[String, Any]
 }
+
+abstract class StringProviderRule(inVal: StringProvider) {
+
+  def runRule(columnIndex: Int, row: Row, schema: Schema, matcher: (String, String) => Boolean): ValidationNEL[String, Any] = {
+    val columnDefinition = schema.columnDefinitions(columnIndex)
+
+    inVal.cellValue(columnIndex, row, schema) match {
+      case Left(e) => e.failNel[Any]
+
+      case Right(ruleValue) => {
+        val cellValue = row.cells(columnIndex).value
+        val (cv, rv) = if (schema.columnDefinitions(columnIndex).contains(IgnoreCase())) (cellValue.toLowerCase, ruleValue.toLowerCase) else (cellValue, ruleValue)
+
+        if (matcher(rv,cv))
+          true.successNel[String]
+        else
+          s"in: ${ruleValue} fails for line ${row.lineNumber}, column: ${columnDefinition.id}, value: ${cellValue}".failNel[Any]
+      }
+    }
+  }
+}
+
+case class IsRule(inVal: StringProvider) extends StringProviderRule(inVal) with Rule  {
+
+  override def execute(columnIndex: Int, row: Row, schema: Schema): ValidationNEL[String, Any] = {
+    runRule(columnIndex, row, schema, _.equals(_))
+  }
+}
+
+case class NotRule(inVal: StringProvider) extends StringProviderRule(inVal) with Rule  {
+
+  override def execute(columnIndex: Int, row: Row, schema: Schema): ValidationNEL[String, Any] = {
+    runRule(columnIndex, row, schema, !_.equals(_))
+  }
+}
+
+case class InRule(inVal: StringProvider) extends StringProviderRule(inVal) with Rule  {
+
+  override def execute(columnIndex: Int, row: Row, schema: Schema): ValidationNEL[String, Any] = {
+    runRule(columnIndex, row, schema, _.contains(_))
+  }
+}
+
+case class StartsRule(inVal: StringProvider) extends StringProviderRule(inVal) with Rule  {
+
+  override def execute(columnIndex: Int, row: Row, schema: Schema): ValidationNEL[String, Any] = {
+    runRule(columnIndex, row, schema, _.startsWith(_))
+  }
+}
+
+case class EndsRule(inVal: StringProvider) extends StringProviderRule(inVal) with Rule {
+
+  override def execute(columnIndex: Int, row: Row, schema: Schema): ValidationNEL[String, Any] = {
+    runRule(columnIndex, row, schema, _.endsWith(_))
+  }
+}
