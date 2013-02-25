321dfae20fe24ddbe2f9ded542ea1d32e91121ac
Benjamin Parker <ben@benparker.co.uk>
2013-02-25 12:10:29 +0000

    [BP] - improved type on fail
    git-svn-id: https://wb-d-tfs2.web.local/svn/dri/trunk/csv-validator@4929 6bcde575-cb03-8b4d-b1e0-7a084bef6d9c

--- csv-validator-core/src/main/scala/uk/gov/tna/dri/validator/MetaDataValidator.scala
+++ csv-validator-core/src/main/scala/uk/gov/tna/dri/validator/MetaDataValidator.scala
@@ -13,9 +13,9 @@ trait MetaDataValidator {
 
   def validate(csv: Reader, schema: Schema): MetaDataValidation[Any] = {
     val rows = new CSVReader(csv).readAll().toList
-    if (rows.isEmpty) "metadata file is empty".failNel else {
+    if (rows.isEmpty) "metadata file is empty".failNel[Any] else {
       val rowsWithNoHeader = if (schema.globalDirectives.contains(NoHeader())) rows else rows.drop(1)
-      if (rowsWithNoHeader.isEmpty) "metadata file has a header but no data".failNel
+      if (rowsWithNoHeader.isEmpty) "metadata file has a header but no data".failNel[Any]
       else validateRows(rowsWithNoHeader.map(_.toList).zipWithIndex.map(r => Row(r._1.map(Cell(_)), r._2 + 1)), schema)
     }
   }
