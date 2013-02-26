5c613ec1423ab8ccea81a58fcb0765cec912853f
Andy Hicks <andy@hicks.net>
2013-02-26 12:18:50 +0000

    [AH] - more checksum test
    git-svn-id: https://wb-d-tfs2.web.local/svn/dri/trunk/csv-validator@4958 6bcde575-cb03-8b4d-b1e0-7a084bef6d9c

--- csv-validator-core/src/test/scala/uk/gov/tna/dri/validator/MetaDataValidatorChecksumSpec.scala
+++ csv-validator-core/src/test/scala/uk/gov/tna/dri/validator/MetaDataValidatorChecksumSpec.scala
@@ -28,13 +28,13 @@ class MetaDataValidatorChecksumSpec extends Specification {
 
   import TestMetaDataValidator._
 
-  "Validation" should {
 
-    "succeed when calculated algorithm does match given cross referenced string value - with /" in {
+  "Checksum with path/filename in schema" should {
+    "succeed when calculated algorithm does match given value" in {
       val schema =
         """@totalColumns 2 @noHeader
            File:
-           MD5: checksum(file($File), "MD5")
+           MD5: checksum(file("src/test/resources/uk/gov/tna/dri/schema/checksum.txt"),"MD5")
         """
 
       val metaData = """src/test/resources/uk/gov/tna/dri/schema/checksum.txt,232762380299115da6995e4c4ac22fa2"""
@@ -42,11 +42,102 @@ class MetaDataValidatorChecksumSpec extends Specification {
       validate(metaData, schema) must beLike { case Success(_) => ok }
     }
 
-    "fail when calculated algorithm does not match given cross referenced string value - with /" in {
+    "fail when calculated algorithm does match not given string value" in {
       val schema =
         """@totalColumns 2 @noHeader
            File:
-           MD5: checksum(file($File), "MD5")
+           MD5: checksum(file("src/test/resources/uk/gov/tna/dri/schema/checksum.txt"),"MD5")
+        """
+
+      val metaData = """src/test/resources/uk/gov/tna/dri/schema/checksum.txt,wrong"""
+
+      validate(metaData, schema) must beLike {
+        case Failure(messages) => messages.list mustEqual List("""checksum(file("src/test/resources/uk/gov/tna/dri/schema/checksum.txt")) fails for line: 1, column: MD5, value: wrong""")
+      }
+    }
+
+  }
+
+
+  "Checksum with rootpath and filename in schema" should {
+    "succeed when calculated algorithm does match given cross referenced string value" in {
+      val schema =
+        """@totalColumns 2 @noHeader
+           File:
+           MD5: checksum(file("src/test/resources/uk/gov/tna/dri/schema","checksum.txt"),"MD5")
+        """
+
+      val metaData = """ABC,232762380299115da6995e4c4ac22fa2"""
+
+      validate(metaData, schema) must beLike { case Success(_) => ok }
+    }
+
+//    "fail when calculated algorithm does match given cross referenced string value" in {
+//      val schema =
+//        """@totalColumns 2 @noHeader
+//           File:
+//           MD5: checksum(file("src/test/resources/uk/gov/tna/dri/schema","checksum.txt"),"MD5")
+//        """
+//
+//      val metaData = """ABC,wrong"""
+//
+//      validate(metaData, schema) must beLike {
+//        case Failure(messages) => messages.list mustEqual List("""checksum(file("src/test/resources/uk/gov/tna/dri/schema","checksum.txt")) fails for line: 1, column: MD5, value: wrong""")
+//      }
+//    }
+
+  }
+
+  "Checksum with root in schema and file in metadata" should {
+
+    "succeed when calculated algorithm does match given root & cross referenced string value" in {
+      val schema =
+        """@totalColumns 2 @noHeader
+           File:
+           MD5: checksum(file("src/test/resources/uk/gov/tna/dri/schema",$File),"MD5")
+        """
+
+      val metaData = """checksum.txt,232762380299115da6995e4c4ac22fa2"""
+
+      validate(metaData, schema) must beLike { case Success(_) => ok }
+    }
+
+    //    "fail when incorrect root given in schema for root" in {
+    //      val schema =
+    //        """@totalColumns 2 @noHeader
+    //           File:
+    //           MD5: checksum(file("invalid/path/to/root",$File), "MD5")
+    //        """
+    //
+    //      val metaData = """checksum.txt,232762380299115da6995e4c4ac22fa2"""
+    //
+    //      validate(metaData, schema) must beLike {
+    //        case Failure(messages) => messages.list mustEqual List("""checksum(file("invalid/path/to/root"),($File)) fails for line: 1, column: MD5, value: rubbish""")
+    //      }
+    //    }
+
+
+
+
+    "Checksum with fullpath in metedata" should {
+
+    "succeed when calculated algorithm does match given cross referenced string value" in {
+      val schema =
+        """@totalColumns 2 @noHeader
+           File:
+           MD5: checksum(file($File),"MD5")
+        """
+
+      val metaData = """src/test/resources/uk/gov/tna/dri/schema/checksum.txt,232762380299115da6995e4c4ac22fa2"""
+
+      validate(metaData, schema) must beLike { case Success(_) => ok }
+    }
+
+    "fail when calculated algorithm does not match given cross referenced string value" in {
+      val schema =
+        """@totalColumns 2 @noHeader
+           File:
+           MD5: checksum(file($File),"MD5")
         """
 
       val metaData = """src/test/resources/uk/gov/tna/dri/schema/checksum.txt,rubbish"""
@@ -55,30 +146,101 @@ class MetaDataValidatorChecksumSpec extends Specification {
         case Failure(messages) => messages.list mustEqual List("checksum(file($File)) fails for line: 1, column: MD5, value: rubbish")
       }
     }
+  }
 
-    "succeed when calculated algorithm does match given root & cross referenced string value - with /" in {
+    "fail when calculated algorithm does match" in {
       val schema =
         """@totalColumns 2 @noHeader
            File:
-           MD5: checksum(file("src/test/resources/uk/gov/tna/dri/schema",$File), "MD5")
+           MD5: checksum(file("src/test/resources/uk/gov/tna/dri/schema",$File),"MD5")
         """
 
-      val metaData = """checksum.txt,232762380299115da6995e4c4ac22fa2"""
+      val metaData = """checksum.txt,rubbish"""
 
-      validate(metaData, schema) must beLike { case Success(_) => ok }
+      validate(metaData, schema) must beLike {
+        case Failure(messages) => messages.list mustEqual List("""checksum(file("src/test/resources/uk/gov/tna/dri/schema"), ($File)) fails for line: 1, column: MD5, value: rubbish""")
+      }
     }
 
-    "succeed when calculated algorithm does match given cross referenced root & cross referenced string value - with /" in {
+  }
+
+
+  "Checksum with cross reference on both root and file" should {
+
+    "succeed when root and file referance a valid file" in {
       val schema =
         """@totalColumns 3 @noHeader
            Root:
            File:
-           MD5: checksum(file($Root,$File), "MD5")
+           MD5: checksum(file($Root,$File),"MD5")
         """
 
       val metaData = """src/test/resources/uk/gov/tna/dri/schema,checksum.txt,232762380299115da6995e4c4ac22fa2"""
 
       validate(metaData, schema) must beLike { case Success(_) => ok }
     }
+
+//    "fail when incorrect root given in metadata" in {
+//      val schema =
+//        """@totalColumns 3 @noHeader
+//           Root:
+//           File:
+//           MD5: checksum(file($Root,$File), "MD5")
+//        """
+//
+//      val metaData = """invalid/path/to/root,checksum.txt,232762380299115da6995e4c4ac22fa2"""
+//
+//      validate(metaData, schema) must beLike {
+//        case Failure(messages) => messages.list mustEqual List("""checksum(file("src/test/resources/uk/gov/tna/dri/schema"),($File)) fails for line: 1, column: MD5, value: rubbish""")
+//      }
+//    }
   }
+
+
+
+  "Checksum with an algorithm" should {
+
+    "succeed when using a valid algorithm" in {
+      val schema =
+        """@totalColumns 2 @noHeader
+           File:
+           MD5: checksum(file($File),"MD5")
+        """
+
+      val metaData = """src/test/resources/uk/gov/tna/dri/schema/checksum.txt,232762380299115da6995e4c4ac22fa2"""
+
+      validate(metaData, schema) must beLike { case Success(_) => ok }
+    }
+
+//    "fail when algorithm is invalid/unknown" in {
+//      val schema =
+//        """@totalColumns 2 @noHeader
+//             File:
+//             MD5: checksum(file($File), "INVALID")
+//        """
+//
+//      val metaData = """src/test/resources/uk/gov/tna/dri/schema/checksum.txt,232762380299115da6995e4c4ac22fa2"""
+//
+//      validate(metaData, schema) must beLike {
+//        case Failure(messages) => messages.list mustEqual List("checksum(file($File)) fails for line: 1, column: MD5, value: rubbish")
+//      }
+//    }
+  }
+
+
+//  "Checksum with multi files matches" should {
+//    "succeed with 3 files" in {
+//      val schema =
+//        """@totalColumns 2 @noHeader
+//           File:
+//           MD5: checksum(file("src/test/resources/uk/gov/tna/dri/schema", $File), "MD5")
+//        """
+//
+//      val metaData = """*.txt,"232762380299115da6995e4c4ac22fa2,232762380299115da6995e4c4ac22fa2,232762380299115da6995e4c4ac22fa2""""
+//
+//      validate(metaData, schema) must beLike { case Success(_) => ok }
+//
+//    }
+//  }
+
 }
