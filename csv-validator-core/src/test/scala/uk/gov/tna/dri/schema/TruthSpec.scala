688da765b5f3de19bdd5aa953197b6aa99f1622e
Benjamin Parker <ben@benparker.co.uk>
2013-02-27 18:20:48 +0000

    [BP] - The Truth Spec
    git-svn-id: https://wb-d-tfs2.web.local/svn/dri/trunk/csv-validator@5049 6bcde575-cb03-8b4d-b1e0-7a084bef6d9c

---
+++ csv-validator-core/src/test/scala/uk/gov/tna/dri/schema/TruthSpec.scala
@@ -0,0 +1,38 @@
+package uk.gov.tna.dri.schema
+
+import org.specs2.mutable.Specification
+import org.specs2.matcher.DataTables
+
+class TruthSpec extends Specification with DataTables {
+
+  "logic" should {
+
+    "work for a OR b AND c" in {
+
+           "a"  |  "b"  | "c"   | "d"   |
+          false ! false ! false ! false |
+          true  ! false ! false ! true  |
+          true  ! true  ! false ! true  |
+          false ! true  ! false ! false |
+          false ! true  ! true  ! true  |
+          true  ! true  ! true  ! true  |> {
+
+          (a, b, c, d) =>  a || b && c must_== d
+        }
+    }
+
+    "work for a AND b OR c" in {
+
+         "a"  |  "b"  | "c"   | "d"   |
+        false ! false ! false ! false |
+        true  ! false ! false ! false |
+        true  ! true  ! false ! true  |
+        false ! true  ! false ! false |
+        false ! true  ! true  ! true  |
+        true  ! true  ! true  ! true  |> {
+
+        (a, b, c, d) =>  a && b || c must_== d
+      }
+    }
+  }
+}
