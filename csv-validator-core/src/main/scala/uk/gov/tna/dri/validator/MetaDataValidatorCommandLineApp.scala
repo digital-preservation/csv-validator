8665cfb17cf0dbf412b711f221c55b55eb76c55d
Andy Hicks <andy@hicks.net>
2013-03-08 09:40:38 +0000

    [AH] - command line interpreter rewrite. Added  --path
    git-svn-id: https://wb-d-tfs2.web.local/svn/dri/trunk/csv-validator@5435 6bcde575-cb03-8b4d-b1e0-7a084bef6d9c

--- csv-validator-core/src/main/scala/uk/gov/tna/dri/validator/MetaDataValidatorCommandLineApp.scala
+++ csv-validator-core/src/main/scala/uk/gov/tna/dri/validator/MetaDataValidatorCommandLineApp.scala
@@ -12,70 +12,85 @@ object  SystemExits {
   val InvalidSchema = 2
   val InvalidCsv = 3
 }
+
 object MetaDataValidatorCommandLineApp extends App {
   type AppValidation[S] = ValidationNEL[String, S]
 
-  val exitCode = run(args)
+  val (exitMsg, exitCode) = run(args)
+  println(exitMsg)
   System.exit(exitCode)
 
-  def run(args: Array[String]): Int = {
+  def run(args: Array[String]): (String,Int) = {
 
-    val pathSubstitutionsList: List[(String,String)] = findSubstitutionPaths(args.toList)
+    val result = for {
+      ((metaDataFile, schemaFile), remainderArgs) <- findFiles(args.toList)
+      (failFast, remainderArgs1) <- findFailFast(remainderArgs)
+      (pathSubstitutionsList, remainderArgs2) <- findPaths(remainderArgs1)
+      (_,_) <- unknownParams(remainderArgs2)
+    } yield  {
+      processMetaData(metaDataFile, schemaFile, failFast, pathSubstitutionsList)
+    }
 
-    val (failFast, fileArgs) = failFastAndFileArgs(args.toList)
+    result match {
+      case Right(r) => (r._1, r._2)
+      case Left(errMsg) => (errMsg, SystemExits.IncorrectArguments)
+    }
+  }
 
-    checkFileArguments(fileArgs) match {
+  private def createValidator(failFast: Boolean, pathSubstitutionsList: List[(String,String)]) =
+    if (failFast) new MetaDataValidatorApp with FailFastMetaDataValidator { val pathSubstitutions = pathSubstitutionsList }
+    else new MetaDataValidatorApp with AllErrorsMetaDataValidator { val pathSubstitutions = pathSubstitutionsList }
 
-      case FailureZ(errors) => println(prettyPrint(errors)); SystemExits.IncorrectArguments
 
-      case SuccessZ(_) => {
-        val (metaDataFile, schemaFile) = inputFilePaths(fileArgs)
-        println("Validating...")
-
-        val validator = if (failFast) new MetaDataValidatorApp with FailFastMetaDataValidator { val pathSubstitutions = pathSubstitutionsList } else new MetaDataValidatorApp with AllErrorsMetaDataValidator { val pathSubstitutions = pathSubstitutionsList }
-
-        validator.parseSchema(schemaFile) match {
-          case FailureZ(errors) => println(prettyPrint(errors)); SystemExits.InvalidSchema
-          case SuccessZ(schema) => validator.validate(metaDataFile, schema) match {
-            case FailureZ(errors) => println(prettyPrint(errors)); SystemExits.InvalidCsv
-            case SuccessZ(_) => println("PASS"); SystemExits.ValidCsv
-          }
+  private def processMetaData(metaDataFile: String, schemaFile: String, failFast: Boolean, pathSubstitutionsList: List[(String,String)] ) = {
+    val validator = createValidator (failFast, pathSubstitutionsList)
+    validator.parseSchema(schemaFile) match {
+      case FailureZ(errors) => (prettyPrint(errors), SystemExits.InvalidSchema)
+      case SuccessZ(schema) =>
+        validator.validate(metaDataFile, schema) match {
+          case FailureZ(errors) => (prettyPrint(errors), SystemExits.InvalidCsv)
+          case SuccessZ(_) => ("PASS", SystemExits.ValidCsv)
         }
+    }
+  }
+
+
+  def findFailFast(args: List[String]): Either[String, (Boolean, List[String])] = {
+    val (flags, remainder) = args.partition( a => a.startsWith("--fail-fast") || a == "-f")
+    Right((!flags.isEmpty, remainder ))
+  }
+
+
+  def findFiles(args: List[String]): Either[String, ((String,String), List[String])] = {
+    if ( args.length < 2)   Left(usage)
+    else {
+      val files = args.takeRight(2)
+      checkFilesReadable(files) match {
+        case FailureZ(errors) => Left(prettyPrint(errors))
+        case SuccessZ(_) =>
+          val (metaDataFile: String, schemaFile: String) = (files(0),files(1))
+          Right((metaDataFile, schemaFile), args.dropRight(2))
       }
     }
   }
 
-  def checkFileArguments(fileArgs: List[String]): AppValidation[List[String]] = {
-    checkFileArgumentCount(fileArgs) match {
-      case SuccessZ(fargs) => checkFilesReadable(fargs)
-      case fail => fail
+  def findPaths(args: List[String]): Either[String, (List[(String,String)], List[String])] =
+    if ( args.isEmpty ) Right(List.empty,args)
+    else if (args.length < 3 ) { if( args.contains("--path")) Left("Missing param to --path\n" + usage) else Right(List.empty,args) }
+    else {
+      val argsWithIndex = args.zipWithIndex
+      val path = argsWithIndex.sliding(3).filter{  i => i(0)._1 == "--path"}.map( x => (x(1), x(2))).toList
+      val remain = argsWithIndex.filter{ i =>  path.filter{pf => pf._1 == i || pf._2 == i}.isEmpty }
+      Right(path.map( i => (i._1._1, i._2._1) ),remain.filterNot(_._1 == "--path").map(_._1))
     }
-  }
 
-  private def checkFileArgumentCount(fileArgs: List[String]) = {
-    if (!fileArgumentCountValid(fileArgs)) usage.failNel[List[String]]
-    else fileArgs.successNel[String]
-  }
 
-  def failFastAndFileArgs(args: List[String]) = {
-    val (flags, files) = args.partition( a => a.startsWith("-")) // ==  "--fail-fast" || a == "-f")
-    (flags.contains( "--fail-fast" ) || flags.contains( "-f" ), files)
-  }
+  def unknownParams(args: List[String]): Either[String, (List[(String,String)], List[String])] =
+    if (!args.isEmpty) Left(s"Unknown parameter ${args.mkString(", ")}")
+    else Right(List.empty, args)
 
-  private def fileArgumentCountValid(fileArgs: List[String]) = fileArgs.length == 2
 
-  def findSubstitutionPaths(args: List[String]): List[(String,String)]= {
-    val cmdName = "--path="
-    val paths =  args.filter( a => a.startsWith(cmdName))
-    paths.map( p => {
-      val i = p.drop(cmdName.length).split(",").lift
-      (i(0).getOrElse(""),i(1).getOrElse(""))
-    })
-  }
-
-  private def usage = "Usage: validate [--fail-fast] [--path=<from>,<to>]* <meta-data file path> <schema file path>"
-
-  private def inputFilePaths(fileArgs: List[String]) = (fileArgs(0), fileArgs(1))
+  private def usage = """Usage: validate [--fail-fast] [--path <from> <to>]* <meta-data file path> <schema file path>"""
 
   private def checkFilesReadable(fileArgs: List[String]) = fileArgs.map(fileReadable).sequence[AppValidation, String]
 
@@ -83,7 +98,7 @@ object MetaDataValidatorCommandLineApp extends App {
 
   private def fileNotReadableMessage(filePath: String) = "Unable to read file : " + filePath
 
-  private def prettyPrint(l: NonEmptyList[String]) = l.list.mkString(sys.props("line.separator"))
+  private def prettyPrint(l: NonEmptyList[String]): String = l.list.mkString(sys.props("line.separator"))
 }
 
 trait MetaDataValidatorApp extends SchemaParser {
--- csv-validator-core/src/test/scala/uk/gov/tna/dri/validator/MetaDataValidatorAppSpec.scala
+++ csv-validator-core/src/test/scala/uk/gov/tna/dri/validator/MetaDataValidatorAppSpec.scala
@@ -11,44 +11,44 @@ class MetaDataValidatorAppSpec extends Specification {
 
   "Check arguments" should {
     "give usage message when no arguments supplied" in {
-      MetaDataValidatorCommandLineApp.checkFileArguments(Nil) must beLike {
-        case Failure(errors) => errors.list mustEqual List("Usage: validate [--fail-fast] [--path=<from>,<to>]* <meta-data file path> <schema file path>")
+      MetaDataValidatorCommandLineApp.run(Array.empty) must beLike {
+        case (errMsg,errCode) => errMsg mustEqual """Usage: validate [--fail-fast] [--path <from> <to>]* <meta-data file path> <schema file path>"""
       }
     }
 
     "give usage message when one argument supplied" in {
-      MetaDataValidatorCommandLineApp.checkFileArguments(List("meta file")) must beLike {
-        case Failure(errors) => errors.list mustEqual List("Usage: validate [--fail-fast] [--path=<from>,<to>]* <meta-data file path> <schema file path>")
+      MetaDataValidatorCommandLineApp.run(Array("meta file")) must beLike {
+        case (errMsg,errCode) => errMsg mustEqual """Usage: validate [--fail-fast] [--path <from> <to>]* <meta-data file path> <schema file path>"""
       }
     }
 
-    "give usage message when too many arguments supplied" in {
-      MetaDataValidatorCommandLineApp.checkFileArguments(List("somMetaData.csv", "someSchema.txt", "something extra")) must beLike {
-        case Failure(errors) => errors.list mustEqual List("Usage: validate [--fail-fast] [--path=<from>,<to>]* <meta-data file path> <schema file path>")
-      }
-    }
+//    "give usage message when too many arguments supplied" in {
+//      MetaDataValidatorCommandLineApp.run(List("somMetaData.csv", "someSchema.txt", "something extra").toArray) must beLike {
+//        case (errMsg,errCode) => errMsg mustEqual """Usage: validate [--fail-fast] [--path <from> <to>]* <meta-data file path> <schema file path>"""
+//      }
+//    }
 
     "fail if metadata file is unreadable" in {
-      MetaDataValidatorCommandLineApp.checkFileArguments(List("nonExistentMetaData.csv", basePath + "schema.txt")) must beLike {
-        case Failure(errors) => errors.list mustEqual List("Unable to read file : nonExistentMetaData.csv")
+      MetaDataValidatorCommandLineApp.run(Array("nonExistentMetaData.csv", basePath + "schema.txt")) must beLike {
+        case (errMsg,errCode) => errMsg mustEqual "Unable to read file : nonExistentMetaData.csv"
       }
     }
 
     "fail if schema file is unreadable" in {
-      MetaDataValidatorCommandLineApp.checkFileArguments(List(basePath + "metaData.csv", "nonExistentSchema.txt")) must beLike {
-        case Failure(errors) => errors.list mustEqual List("Unable to read file : nonExistentSchema.txt")
+      MetaDataValidatorCommandLineApp.run(Array(basePath + "metaData.csv", "nonExistentSchema.txt")) must beLike {
+        case (errMsg,errCode) => errMsg mustEqual "Unable to read file : nonExistentSchema.txt"
       }
     }
 
     "fail if both metadata and schema file are unreadable" in {
-      MetaDataValidatorCommandLineApp.checkFileArguments(List("nonExistentmetaData.csv", "nonExistentSchema.txt")) must beLike {
-        case Failure(errors) => errors.list mustEqual List("Unable to read file : nonExistentmetaData.csv", "Unable to read file : nonExistentSchema.txt")
+      MetaDataValidatorCommandLineApp.run(Array("nonExistentmetaData.csv", "nonExistentSchema.txt")) must beLike {
+        case (errMsg,errCode) => errMsg mustEqual "Unable to read file : nonExistentmetaData.csv\nUnable to read file : nonExistentSchema.txt"
       }
     }
 
     "succeed if both metadata and schema file are readable" in {
-      MetaDataValidatorCommandLineApp.checkFileArguments(List(basePath + "metaData.csv", basePath + "schema.txt")) must beLike {
-        case Success(_) => ok
+      MetaDataValidatorCommandLineApp.run(Array(basePath + "metaData.csv", basePath + "schema.txt")) must beLike {
+        case (errMsg,errCode) => errMsg mustEqual "PASS"
       }
     }
   }
@@ -56,40 +56,95 @@ class MetaDataValidatorAppSpec extends Specification {
   "Fail fast and file args" should {
 
     "return true and the file names for fail fast" in {
-      MetaDataValidatorCommandLineApp.failFastAndFileArgs(List("--fail-fast", "someMetaData.csv", "someSchema.txt")) mustEqual (true, List("someMetaData.csv", "someSchema.txt"))
+      MetaDataValidatorCommandLineApp.run(Array("--fail-fast", "someMetaData.csv", "someSchema.txt")) mustEqual ("Unable to read file : someMetaData.csv\nUnable to read file : someSchema.txt",1)
     }
 
     "return true and the file names for fail fast short form" in {
-      MetaDataValidatorCommandLineApp.failFastAndFileArgs(List("-f", "someMetaData.csv", "someSchema.txt")) mustEqual (true, List("someMetaData.csv", "someSchema.txt"))
+      MetaDataValidatorCommandLineApp.run(Array("-f", "someMetaData.csv", "someSchema.txt")) mustEqual ("Unable to read file : someMetaData.csv\nUnable to read file : someSchema.txt",1)
     }
 
     "return false and the file names for no fail fast" in {
-      MetaDataValidatorCommandLineApp.failFastAndFileArgs(List("someMetaData.csv", "someSchema.txt")) mustEqual (false, List("someMetaData.csv", "someSchema.txt"))
+      MetaDataValidatorCommandLineApp.run(Array("someMetaData.csv", "someSchema.txt")) mustEqual ("Unable to read file : someMetaData.csv\nUnable to read file : someSchema.txt",1)
     }
   }
+
+  "--path and file args" should {
+//    "find " {
+//
+//    }
+
+//    "handle --path option" in {
+//      val commandLine = List[String](
+//        "--path", "c:", "",
+//        "--path", """file://c:""", """file://""",
+//        "--fail-fast"
+//      )
+//      MetaDataValidatorCommandLineApp.findSubstitutionPaths(commandLine) mustEqual Success(List( ("c:", ""), ("file://c:", "file://")) )
+//    }
+
+  }
+
+  "Given a list of args" should {
+   "be able to find all the paths" in {
+     MetaDataValidatorCommandLineApp.findPaths( List("--path", "hello", "world")) mustEqual Right( (List( ("hello", "world") ), List()))
+   }
+
+    "be able to find the path and return remainders" in {
+      MetaDataValidatorCommandLineApp.findPaths( List("123", "--path", "hello", "world", "xyz")) mustEqual Right( (List( ("hello", "world") ), List("123", "xyz")))
+    }
+
+    "be able to find multiple paths and return remainders" in {
+      MetaDataValidatorCommandLineApp.findPaths( List("123", "--path", "hello", "world", "xyz", "--path", "hello2", "world2")) mustEqual Right( (List( ("hello", "world"), ("hello2", "world2") ), List("123", "xyz")))
+    }
+
+
+    "be able to handle a missing value" in {
+      MetaDataValidatorCommandLineApp.findPaths( List("--path", "hello" )) mustEqual Left("Missing param to --path\nUsage: validate [--fail-fast] [--path <from> <to>]* <meta-data file path> <schema file path>")
+    }
+
+    "find the --fail-fast option" in {
+      MetaDataValidatorCommandLineApp.findFailFast( List("--fail-fast", "hello", "world")) mustEqual Right( true, List("hello", "world"))
+    }
+
+    "find find the metafile and schema" in {
+      MetaDataValidatorCommandLineApp.findFiles( List("--fail-fast", basePath + "metaData.csv", basePath + "schema.txt")) mustEqual Right( (basePath + "metaData.csv", basePath + "schema.txt"), List("--fail-fast"))
+    }
+
+
+  }
+
 
   "Command line app" should {
 
     "have exit code 0 when validation successful" in {
-      MetaDataValidatorCommandLineApp.run(Array(basePath + "metaData.csv", basePath + "schema.txt")) mustEqual 0
+      MetaDataValidatorCommandLineApp.run(Array(basePath + "metaData.csv", basePath + "schema.txt")) mustEqual Tuple2("PASS", 0)
     }
+
+//    "have exit code 0 when validation --path successful" in {
+//      MetaDataValidatorCommandLineApp.run(Array( "--path", "c:", "", basePath + "metaData.csv", basePath + "schema.txt"))._2 mustEqual 0
+//    }
 
     "have exit code 1 when the command line arguments are wrong" in {
-      MetaDataValidatorCommandLineApp.run(Array("")) mustEqual 1
+      MetaDataValidatorCommandLineApp.run(Array(""))._2 mustEqual 1
     }
 
+    "have exit code 1 when the --path is missing one args " in {
+      MetaDataValidatorCommandLineApp.run(Array("--path", "c:", basePath + "metaData.csv", basePath + "schema.txt"))._2 mustEqual 1
+    }
+
+    "have exit code 1 when the --path is missing both args " in {
+      MetaDataValidatorCommandLineApp.run(Array("--path", basePath + "metaData.csv", basePath + "schema.txt"))._2 mustEqual 1
+    }
+
+
     "have exit code 2 when the schema is invalid" in {
-      MetaDataValidatorCommandLineApp.run(Array(basePath + "metaData.csv", basePath + "badSchema.txt")) mustEqual 2
+      MetaDataValidatorCommandLineApp.run(Array(basePath + "metaData.csv", basePath + "badSchema.txt"))._2 mustEqual 2
     }
 
     "have exit code 3 when the metadata is invalid" in {
-      MetaDataValidatorCommandLineApp.run(Array(basePath + "acceptance/standardRulesFailMetaData.csv", basePath + "acceptance/standardRulesSchema.txt")) mustEqual 3
+      MetaDataValidatorCommandLineApp.run(Array(basePath + "acceptance/standardRulesFailMetaData.csv", basePath + "acceptance/standardRulesSchema.txt"))._2 mustEqual 3
     }
 
-    "handle --path option" in {
-      val commandLine = List[String]("""--path=c:,""", """--path=file://c:,file://""", "--fail-fast")
-      MetaDataValidatorCommandLineApp.findSubstitutionPaths(commandLine) mustEqual List( ("c:", ""), ("file://c:", "file://") )
-    }
   }
 
   "Parsing schema" should {
