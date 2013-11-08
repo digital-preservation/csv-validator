c1422d33b9476a069c85fa2ad1f3e339ecbb9a94
Adam Retter <adam.retter@googlemail.com>
2013-11-08 13:27:29 +0000

    [license] Enforced license headers to MPL v2.0

--- csv-validator-core/src/main/scala/uk/gov/tna/dri/csv/validator/schema/SchemaParser.scala
+++ csv-validator-core/src/main/scala/uk/gov/tna/dri/csv/validator/schema/SchemaParser.scala
@@ -1,5 +1,6 @@
-/*
- * Copyright (c) 2013, The National Archives digitalpreservation@nationalarchives.gov.uk
+/**
+ * Copyright (c) 2013, The National Archives <digitalpreservation@nationalarchives.gov.uk>
+ * http://www.nationalarchives.gov.uk
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
--- csv-validator-java-api/src/main/java/uk/gov/tna/dri/csv/validator/api/java/WarningMessage.java
+++ csv-validator-java-api/src/main/java/uk/gov/tna/dri/csv/validator/api/java/WarningMessage.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2013, The National Archives <digitalpreservation@nationalarchives.gov.uk>
+ * http://www.nationalarchives.gov.uk
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package uk.gov.tna.dri.csv.validator.api.java;
 
 public class WarningMessage extends FailMessage {
